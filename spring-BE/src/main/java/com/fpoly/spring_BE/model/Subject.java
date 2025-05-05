b941a6e31da9d6baadd452356cdfc52c1255dee9
ntl1997 <luandajk4@gmail.com>
2025-05-05 21:38:24 +0700

    add Subject model with relationships and attributes

---
+++ spring-BE/src/main/java/com/fpoly/spring_BE/model/Subject.java
@@ -0,0 +1,27 @@
+package com.fpoly.spring_BE.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "Subjects")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Subject {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Id")
+    private int id;
+
+    @Column(name = "Name", nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "subject", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Class> classes;
+}
