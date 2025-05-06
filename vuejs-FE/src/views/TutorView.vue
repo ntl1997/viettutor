df1691a68222572b858dd0682b2dd34da0f3f7a4
Tunmbg <phamtungzgzg@gmail.com>
2025-05-06 09:23:16 +0700

    Footer của trang gia sư

--- vuejs-FE/src/views/TutorView.vue
+++ vuejs-FE/src/views/TutorView.vue
@@ -1,13 +1,20 @@
 <template>
-  <IndexNavbar />
-  <div>
-    <h1>Trang gia sư</h1>
+  <div class="flex flex-col min-h-screen">
+    <IndexNavbar />
+
+    <div class="flex-grow">
+      <h1 class="text-center text-2xl mt-4">Trang Gia Sư</h1>
+    </div>
+
+    <FooterDown />
   </div>
 </template>
 <script>
 import IndexNavbar from '../components/Navbars/IndexNavbar.vue'
+import FooterDown from '../components/Footers/Footer.vue'
+
 export default {
   name: 'TutorView',
-  components: { IndexNavbar },
+  components: { IndexNavbar, FooterDown },
 }
 </script>
