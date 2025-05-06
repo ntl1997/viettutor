8361ac04903ce1ee3b3c57c339c606dfe47cca80
ntl1997 <luandajk4@gmail.com>
2025-05-06 10:39:55 +0700

    add @JsonIgnore annotation to Subject class for class relationships

--- spring-BE/src/main/java/com/fpoly/spring_BE/model/Subject.java
+++ spring-BE/src/main/java/com/fpoly/spring_BE/model/Subject.java
@@ -7,6 +7,8 @@ import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 @Table(name = "Subjects")
 @Data
@@ -23,5 +25,6 @@ public class Subject {
     private String name;
 
     @OneToMany(mappedBy = "subject", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnore
     private List<Class> classes;
 }
