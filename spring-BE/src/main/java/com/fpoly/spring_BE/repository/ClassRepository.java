f32ebcab340cbd854ce9b9e8c80993335349d16f
HieuNT-2001-Student <hieuntpp03096@fpt.edu.vn>
2025-05-08 10:42:43 +0700

    add ClassService

--- spring-BE/src/main/java/com/fpoly/spring_BE/repository/ClassRepository.java
+++ spring-BE/src/main/java/com/fpoly/spring_BE/repository/ClassRepository.java
@@ -2,7 +2,6 @@ package com.fpoly.spring_BE.repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,13 +11,13 @@ import com.fpoly.spring_BE.model.Class;
 public interface ClassRepository extends JpaRepository<Class, Integer> {
     // Additional query methods if needed
 
-    Page<Class> findByStatus(Pageable pageable, Sort sortBy, String status);
+    Page<Class> findByStatus(Pageable pageable, String status);
 
-    Page<Class> findBySubjectAndStatus(Pageable pageable, Sort sortBy, String status, int subjectId);
+    Page<Class> findBySubjectAndStatus(Pageable pageable, String status, int subjectId);
 
-    Page<Class> findByLevelAndStatus(Pageable pageable, Sort sortBy, String status, int levelId);
+    Page<Class> findByLevelAndStatus(Pageable pageable, String status, int levelId);
 
-    Page<Class> findByLocaltionAndStatus(Pageable pageable, Sort sortBy, String status, int locationId);
+    Page<Class> findByLocaltionAndStatus(Pageable pageable, String status, int locationId);
 
-    Page<Class> findBylearningModeAndStatus(Pageable pageable, Sort sortBy, String status, String learningMode);
+    Page<Class> findBylearningModeAndStatus(Pageable pageable, String status, String learningMode);
 }
---
+++ spring-BE/src/main/java/com/fpoly/spring_BE/service/ClassService.java
@@ -0,0 +1,71 @@
+package com.fpoly.spring_BE.service;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import com.fpoly.spring_BE.model.Class;
+import com.fpoly.spring_BE.repository.ClassRepository;
+
+@Service
+public class ClassService {
+
+    private final ClassRepository classRepository;
+
+    public ClassService(ClassRepository classRepository) {
+        this.classRepository = classRepository;
+    }
+
+    public Optional<Class> getClassById(int id) {
+        return classRepository.findById(id);
+    }
+
+    public Page<Class> getAllClasses(int page, int size) {
+        Sort sort = Sort.by("createdAt").descending();
+        Pageable pageable = PageRequest.of(page, size, sort);
+        return classRepository.findAll(pageable);
+    }
+
+    public Page<Class> getClassesByStatus(int page, int size) {
+        Sort sort = Sort.by("createdAt").descending();
+        Pageable pageable = PageRequest.of(page, size, sort);
+        return classRepository.findByStatus(pageable, "open");
+    }
+
+    public Page<Class> getClassesBySubjectAndStatus(int page, int size, int subjectId) {
+        Sort sort = Sort.by("createdAt").descending();
+        Pageable pageable = PageRequest.of(page, size, sort);
+        return classRepository.findBySubjectAndStatus(pageable, "open", subjectId);
+    }
+
+    public Page<Class> getClassesByLevelAndStatus(int page, int size, int levelId) {
+        Sort sort = Sort.by("createdAt").descending();
+        Pageable pageable = PageRequest.of(page, size, sort);
+        return classRepository.findByLevelAndStatus(pageable, "open", levelId);
+    }
+
+    public Page<Class> getClassesByLocationAndStatus(int page, int size, int locationId) {
+        Sort sort = Sort.by("createdAt").descending();
+        Pageable pageable = PageRequest.of(page, size, sort);
+        return classRepository.findByLocaltionAndStatus(pageable, "open", locationId);
+    }
+
+    public Page<Class> getClassesByLearningModeAndStatus(int page, int size, String learningMode) {
+        Sort sort = Sort.by("createdAt").descending();
+        Pageable pageable = PageRequest.of(page, size, sort);
+        return classRepository.findBylearningModeAndStatus(pageable, "open", learningMode);
+    }
+
+    public Class saveClass(Class aClass) {
+        return classRepository.save(aClass);
+    }
+
+    public void deleteClass(int id) {
+        classRepository.deleteById(id);
+    }
+    
+}
