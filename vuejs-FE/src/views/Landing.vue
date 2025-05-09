738977f815e63a0c29b18375a434918b7a746b1e
Nguyễn Kim Dũng <nguyenkimdung1501@gmail.com>
2025-05-09 18:04:09 +0700

    Edit icon

--- vuejs-FE/src/views/Landing.vue
+++ vuejs-FE/src/views/Landing.vue
@@ -120,7 +120,21 @@
                   <div
                     class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-lightBlue-400"
                   >
-                    <i class="fas fa-retweet"></i>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      class="lucide lucide-search-icon lucide-search"
+                    >
+                      <path d="m21 21-4.34-4.34" />
+                      <circle cx="11" cy="11" r="8" />
+                    </svg>
                   </div>
                   <h6 class="text-3xl lg:text-4xl font-bold">
                     Tìm gia sư <span style="color: green">toàn quốc</span>
