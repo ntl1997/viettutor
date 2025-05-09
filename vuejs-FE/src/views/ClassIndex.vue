8c0f89a2c45e20c3a43c7b03457f088bc7e9ba3b
HieuNT-2001 <hieuth0109@gmail.com>
2025-05-09 23:07:29 +0700

    Update ClassIndex.vue

--- vuejs-FE/src/views/ClassIndex.vue
+++ vuejs-FE/src/views/ClassIndex.vue
@@ -36,7 +36,6 @@
           class="inline-block rounded-sm bg-indigo-600 px-8 py-3 text-sm font-medium text-white transition hover:scale-110 hover:shadow-xl focus:ring-3 focus:outline-hidden"
           :to="{
             query: {
-              page: page,
               subjectId: subject,
               levelId: level,
               locationId: location,
