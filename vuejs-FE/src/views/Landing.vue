6c67e458b50f8018f93e6f6a1da28cca2ba99a16
Nguyễn Kim Dũng <nguyenkimdung1501@gmail.com>
2025-05-09 18:08:34 +0700

    Edit icon

--- vuejs-FE/src/views/Landing.vue
+++ vuejs-FE/src/views/Landing.vue
@@ -155,7 +155,22 @@
                   <div
                     class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-emerald-400"
                   >
-                    <i class="fas fa-fingerprint"></i>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      class="lucide lucide-phone-icon lucide-phone"
+                    >
+                      <path
+                        d="M13.832 16.568a1 1 0 0 0 1.213-.303l.355-.465A2 2 0 0 1 17 15h3a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2A18 18 0 0 1 2 4a2 2 0 0 1 2-2h3a2 2 0 0 1 2 2v3a2 2 0 0 1-.8 1.6l-.468.351a1 1 0 0 0-.292 1.233 14 14 0 0 0 6.392 6.384"
+                      />
+                    </svg>
                   </div>
                   <h6 class="text-3xl lg:text-4xl font-bold">
                     Hỗ trợ nhiệt tình <span style="color: green">24/7</span>
